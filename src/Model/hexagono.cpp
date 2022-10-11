61b1a59bb6a9c8cb11136a1b8fcf0f4c3ae15376
Andres <andresfeliped078@gmail.com>
2022-10-11 11:27:42 -0500

    NEW: Se creo la clase Hexagono

--- src/Model/hexagono.cpp
+++ src/Model/hexagono.cpp
@@ -1,3 +1,16 @@
 //
 // Created by andre on 11/10/2022.
 //
+#include "hexagono.h"
+
+Hexagono ::Hexagono() {
+    lado = 0;
+    apotema = 0;
+    nombreFigura = "Hexagono";
+}
+
+Hexagono ::Hexagono(float lado, float apotema):Hexagono() {
+    this->lado = lado;
+    this->apotema = apotema;
+}
+
--- src/Model/hexagono.h
+++ src/Model/hexagono.h
@@ -5,4 +5,21 @@
 #ifndef EJERCICIO_POLIMORFISMO_HEXAGONO_H
 #define EJERCICIO_POLIMORFISMO_HEXAGONO_H
 
+#include "FiguraGeometrica.h"
+#include <cmath>
+#include <iostream>
+
+class Hexagono : public FiguraGeometrica {
+    private:
+        float lado;
+        float apotema;
+    public:
+        Hexagono();
+        virtual ~Hexagono() = default;
+        Hexagono(float lado, float apotema);
+        void dibujarFigura() override;
+        float calcularArea() override;
+        float calcularPerimetro() override;
+};
+
 #endif //EJERCICIO_POLIMORFISMO_HEXAGONO_H
